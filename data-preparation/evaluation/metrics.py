3c7a910ae755445b825719e47170a744423d43d5
Nasser Eddine Monir <nasser-eddine.monir@inria.fr>
2024-03-28 16:57:43 +0100

    Added a script for computing speech enhancement metrics (SDR, SAR, SIR) with mir_eval.

---
+++ data-preparation/evaluation/metrics.py
@@ -0,0 +1,109 @@
+# import standard libraries
+import os
+import sys
+
+# import third-party libraries
+import librosa
+import numpy as np
+from mir_eval.separation import bss_eval_images
+
+# Compute metrics 
+def metrics(params):
+    """
+    Calculate multichannel speech enhancement metrics at the input stage, the output stage and the delta between them: SDR, SAR, SIR.
+    
+    Parameters
+    ----------
+    params : dict
+        A dictionary containing paths to audio files and metadata.
+        Expected keys are:
+        - 'speech_path': str, path to the clean speech audio file.
+        - 'noise_path': str, path to the noise audio file.
+        - 'mixture_path': str, path to the mixture of speech and noise audio file.
+        - 'estimated_speech_path': str, path to the estimated speech audio file.
+        - 'filename': str, the name of the audio file being evaluated.
+        - 'model': str, the name of the model used for speech enhancement.
+        - 'scenario': str, a description of the scenario or conditions under which the audio was processed.
+        
+    Returns
+    -------
+    metrics_summary : list of dicts
+        A list where each element is a dictionary containing the metrics for each microphone channel ('Front-Left', 'Front-Right').
+        Each dictionary includes:
+        - 'filename': str, the name of the audio file.
+        - 'model': str, the model used for speech enhancement.
+        - 'scenario': str, the scenario description.
+        - 'microphone': str, the microphone channel.
+        - 'sdr_in': float, Signal to Distortion Ratio before enhancement.
+        - 'sdr_out': float, Signal to Distortion Ratio after enhancement.
+        - 'sar_in': float, Signal to Artifacts Ratio before enhancement.
+        - 'sar_out': float, Signal to Artifacts Ratio after enhancement.
+        - 'sir_in': float, Signal to Interference Ratio before enhancement.
+        - 'sir_out': float, Signal to Interference Ratio after enhancement.
+        - 'delta_sdr': float, change in SDR from before to after enhancement.
+        - 'delta_sar': float, change in SAR from before to after enhancement.
+        - 'delta_sir': float, change in SIR from before to after enhancement.
+        
+    Notes
+    -----
+    This function assumes the input and estimated speech signals are stereo and selects channels 0 and 2 for processing.
+    The speech enhancement metrics are computed using the mir_eval library's bss_eval_images function, which is designed for
+    evaluating the quality of speech enhancement (source separation) algorithms.
+    """
+    
+    # Load audios
+    speech = librosa.load(params['speech_path'], sr=None, mono=False)[0][[0, 2]]
+    noise = librosa.load(params['noise_path'], sr=None, mono=False)[0][[0, 2]]
+    mixture = librosa.load(params['mixture_path'], sr=None, mono=False)[0][[0, 2]]
+    estimated_speech_ch0 = librosa.load(params['estimated_speech_path_ch0'], sr=None, mono=False)[0]
+    estimated_speech_ch2 = librosa.load(params['estimated_speech_path_ch2'], sr=None, mono=False)[0]
+    estimated_speech = np.vstack((estimated_speech_ch0, estimated_speech_ch2))
+
+    # Prepare reference and estimated source matrics for metric computation
+    reference      = np.vstack([speech, noise])
+    residual_noise = mixture - estimated_speech
+    estimation_in  = np.vstack([mixture, mixture])
+    estimation_out = np.vstack([estimated_speech, residual_noise])
+
+    # Compute speech enhancement metrics using mir_eval
+    sdr_in, _, sir_in, sar_in, _    = bss_eval_images(reference_sources=reference, estimated_sources=estimation_in, compute_permutation=False)
+    sdr_out, _, sir_out, sar_out, _ = bss_eval_images(reference_sources=reference, estimated_sources=estimation_out, compute_permutation=False)
+
+    # Prepare and return summary of metrics for each microphone channel 
+    metrics_summary = []
+    for i, microphone in enumerate(['Front-Left','Front-Right']):
+        metrics_summary.append({
+            'filename'  : params['filename'],
+            'model'     : params['model'],
+            'scenario'  : params['scenario'],
+            'microphone': microphone,
+            'sdr_in'    : round(sdr_in[i], 4),
+            'sdr_out'   : round(sdr_out[i], 4),
+            'sar_in'    : round(sar_in[i], 4),
+            'sar_out'   : round(sar_out[i], 4),
+            'sir_in'    : round(sir_in[i], 4),
+            'sir_out'   : round(sir_out[i], 4),
+            'delta_sdr' : round(sdr_out[i] - sdr_in[i], 4),
+            'delta_sar' : round(sar_out[i] - sar_in[i], 4),
+            'delta_sir' : round(sir_out[i] - sir_in[i], 4),
+        })
+
+    return metrics_summary
+
+# Main execution block 
+if __name__ == '__main__':
+    # Example params dictionary with paths to the audio files and metadata
+    dataset_path = '/srv/storage/talc3@storage4.nancy.grid5000.fr/multispeech/calcul/users/nmonir/se-ph-eval-plus/'
+    params = {
+        'speech_path'              : os.path.join(dataset_path, 'mix/female/ssn-m/0dBS0N45/121-121726-0000/reverberated-speech.wav'),
+        'noise_path'               : os.path.join(dataset_path, 'mix/female/ssn-m/0dBS0N45/121-121726-0000/reverberated-scaled-noise.wav'),
+        'mixture_path'             : os.path.join(dataset_path, 'mix/female/ssn-m/0dBS0N45/121-121726-0000/mixture.wav'),
+        'estimated_speech_path_ch0': os.path.join(dataset_path, 'estimated_speech/tango/female/ssn-m/0dBS0N45/121-121726-0000/estimated_speech_ch0.wav'),
+        'estimated_speech_path_ch2': os.path.join(dataset_path, 'estimated_speech/tango/female/ssn-m/0dBS0N45/121-121726-0000/estimated_speech_ch2.wav'),
+        'filename'                 : '121-121726-0000',
+        'model'                    : 'tango',
+        'scenario'                 : '0dBS0N45',
+    }
+
+    metrics_summary = metrics(params)
+    print(metrics_summary)
